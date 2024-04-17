cdb7d45f0b8760931c00feb99792aa77f3312154
hjas02 <hyeonho9509@naver.com>
2024-04-17 22:16:01 +0900

    수정

--- HW/HW06/Lecture06-HW/Lecture06-HW/MSList.cpp
+++ HW/HW06/Lecture06-HW/Lecture06-HW/MSList.cpp
@@ -1,19 +1,17 @@
 #include "MSList.h"
 #include <iostream>
 
-// MSNode 클래스의 생성자 구현
+// 생성자 구현
 template <typename T>
 MSNode<T>::MSNode(T data) : data(data), next_node(nullptr) {}
 
-// MSNode 클래스의 소멸자 구현
+// 소멸자 구현
 template <typename T>
 MSNode<T>::~MSNode() {}
 
-// MSList 클래스의 생성자 구현
 template <typename T>
 MSList<T>::MSList() : _head(nullptr), _size(0) {}
 
-// MSList 클래스의 소멸자 구현
 template <typename T>
 MSList<T>::~MSList() {
     MSNode<T>* current = _head;
@@ -24,13 +22,12 @@ MSList<T>::~MSList() {
     }
 }
 
-// MSList 클래스의 get_size() 메서드 구현
+
 template <typename T>
 int MSList<T>::get_size() {
     return _size;
 }
 
-// MSList 클래스의 add() 메서드 구현
 template <typename T>
 void MSList<T>::add(T data) {
     MSNode<T>* new_node = new MSNode<T>(data);
@@ -47,7 +44,6 @@ void MSList<T>::add(T data) {
     _size++;
 }
 
-// MSList 클래스의 operator[] 오버로딩 구현
 template <typename T>
 T& MSList<T>::operator[](int index) {
     if (index < 0 || index >= _size) {
