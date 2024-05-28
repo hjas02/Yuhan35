66b3f16bf26a231107047d5b171201e9bc6f4903
hjas02 <hyeonho9509@naver.com>
2024-05-28 16:04:01 +0900

    루프

---
+++ HW/HW12/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main() {
+    int count = 0;
+    const int time = 1000; // 1초 = 1000 밀리초
+
+    while (count < 100) {
+        auto start = std::chrono::steady_clock::now();
+
+        count++;
+        std::cout << "카운트: " << count;
+
+        auto end = std::chrono::steady_clock::now();        // 루프 종료 시간
+        std::chrono::duration<double, std::milli> elapsed = end - start; // 루프 실행 시간 계산
+
+        std::cout << ", 걸린 시간: " << elapsed.count() << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(time) - elapsed);// 1초 간격을 맞추기 위해 남은 시간 동안 대기
+    }
+
+    return 0;
+}
